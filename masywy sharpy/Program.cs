4096ace727e52d4918ab502ea27f342de4b9348d
merely <merely@DESKTOP-PRBVCO8>
2024-03-09 18:40:43 +0200

    +6.13-6.18

--- masywy sharpy/Program.cs
+++ masywy sharpy/Program.cs
@@ -11,7 +11,7 @@ void printmas(int[,] mas)
     {
         for (int j = 0; j < mas.GetLength(1); j++)
         {
-            Console.Write(mas[i, j]+" ");
+            Console.Write($"{mas[i, j]:D02} ");
         }
         Console.WriteLine("\n");
     }
@@ -454,47 +454,306 @@ Console.WriteLine($"\nСума елементів які кратні 8:{sum}");
 //Задача 6.12
 //Знайти мінімальний елемент серед тих елементів масиву A, які не є елементами масиву B 
 
-int[] randomarray(int[] array,int seed)
+/*int[] randomarray(int[] array, int seed)
 {
     Random random = new Random(seed);
-    for(int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-    array[i] = random.Next(0,200);
-    } 
+        array[i] = random.Next(0, 200);
+    }
     return array;
 }
 Console.WriteLine("Введіть кількість елементів масивів:");
 int size = int.Parse(Console.ReadLine());
 int[] arrayA = new int[size];
 int[] arrayB = new int[size];
-arrayA=randomarray (arrayA,76);
-arrayB=randomarray (arrayB,25);
-int min =arrayA.Min();
-Array.Sort (arrayA);
-Array.Sort (arrayB);
+arrayA = randomarray(arrayA, 76);
+arrayB = randomarray(arrayB, 25);
+int min = arrayA.Min();
+Array.Sort(arrayA);
+Array.Sort(arrayB);
 printarray(arrayA);
 printarray(arrayB);
 
-for(int i = 0;i < arrayA.Length; i++)
+for (int i = 0; i < arrayA.Length; i++)
 {
-    for(int j = 0; j < arrayB.Length; j++)
+    for (int j = 0; j < arrayB.Length; j++)
     {
-        if (arrayA[i]==arrayB[j]){
-          break;
-       
-       }
+        if (arrayA[i] == arrayB[j])
+        {
+            break;
+
+        }
         if (j == arrayB.Length - 1)
         {
             min = arrayA[i];
             Console.WriteLine($"\n{min}");
             return;
         }
-       
+
     }
-} 
+}
+*/
 
-    
-    
-    
+//Задача 6.13
+//Визначити, скільки різних чисел міститься в заданому цілочисельному масиві.
+
+/*int[] randomarray(int[] array, int seed)
+{
+    Random random = new Random(seed);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        array[i] = random.Next(0, 100);
+    }
+    return array;
+}
+Console.WriteLine("Введіть кількість елементів масивів:");
+int size = int.Parse(Console.ReadLine());
+int[] array = new int[size];
+array = randomarray(array, 12);
+printarray(array);
+int temp =0;
+for(int i =0;i<array.Length;i++)
+{  
+    for(int j = 1;j < array.Length; j++)
+    {
+        if(array[j] == array[i] && i!=j)
+        {
+            array[j] = -1;
+            break;
+        }
+    } 
+}
+foreach(int element in array)
+{
+    if (element != -1)
+    {
+        temp++;
+    }
+}
+
+Console.WriteLine("\nКількість різних чисел"+temp);
+*/
+
+//Задача 6.14
+//Написати програму, яка знаходить в масиві значення, що повторюються два і більше разів, і показує їх на екран.
+//Елементи масиву генеруються випадковим чином. Кількість стовпців та рядків вводить користувач. 
+
+/*static int[,] fillmas(int[,] mas)
+{
+    Random random = new Random(2321);   
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i,j] = random.Next(1,100);
+        }
+    }
+    return mas;
+}
+Console.WriteLine("Введіть кількість стовпців");
+int colums = int.Parse(Console.ReadLine());
+Console.WriteLine("Введіть кількість рядків");
+int rows = int.Parse(Console.ReadLine());
+int[,] mas = new int[rows, colums];
+fillmas(mas);
+printmas(mas);
+int temp=-1;
+foreach (int element in mas)
+{
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+            {
+                if (mas[i, j] == element)
+                {           
+                    temp++;
+                    if (temp == 1)
+                    {                    
+                        mas[i,j]*=-1;
+                        break;
+                    }
+                    if (temp > 1)
+                    {
+                        mas[i,j]=0;
+                        break;
+                    }
+                }
+            }   
+    }
+    temp=-1;
+}
+Console.Write($"схожі елементи:   ");
+foreach (int element in mas)
+{
+    if(element<0)
+    {
+        Console.Write($" {element*-1} ");
+    }
+}*/
+
+//Задача 6.15
+//Розробити функцію, що переставляє в зворотному порядку елементи головної діагоналі квадтаної матриці.
+
+/*static int[,] fillmas(int[,] mas)
+{
+    Random random = new Random(2321);   
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i,j] = random.Next(1,100);
+        }
+    }
+    return mas;
+}
+static int[,] reverse(int[,] mas)
+{
+    int temp;
+    for (int i = 0;i < mas.GetLength(0)/2;i++)
+    {
+       temp = mas[i,i];
+       mas[i,i] = mas[mas.GetLength(0)-1-i,mas.GetLength(0)-1-i];
+       mas[mas.GetLength(0)-1-i,mas.GetLength(0)-1-i] = temp;
+        
+    }
+    return mas;
+}
+Console.WriteLine("Введіть розмір");
+int size = int.Parse(Console.ReadLine());
+int[,] mas = new int[size,size];
+fillmas(mas);
+printmas(mas); 
+reverse(mas);
+printmas(mas);*/
+
+//Задача 6.16
+//Переформатувати марицю (двовимірний масив) таким чином, щоб її рядки розміщувалися за зростанням їх поелементних сум.
+
+/*static int[,] fillmas(int[,] mas)
+{
+    Random random = new Random(2321);   
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i,j] = random.Next(1,100);
+        }
+    }
+    return mas;
+}
+Console.WriteLine("Введіть кількість стовпців");
+int colums = int.Parse(Console.ReadLine());
+Console.WriteLine("Введіть кількість рядків");
+int rows = int.Parse(Console.ReadLine());
+int[,] mas = new int[rows, colums];
+fillmas(mas);
+printmas(mas);
+int[] sum =new int[rows];
+for(int i = 0; i < rows; i++)
+{
+    for (int j = 0;j < colums; j++)
+    {
+        sum[i]+=mas[i,j];
+    }
+}
+int n = sum.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (sum[j] > sum[j + 1])
+                {
+                    
+                    int temp = sum[j];
+                    sum[j] = sum[j + 1];
+                    sum[j + 1] = temp;
+                    for(int k = 0; k < colums; k++)
+                    {   
+                        temp = mas[j,k];
+                        mas[j,k] = mas[j+1,k];
+                        mas[j+1,k] = temp;
+                        
+
+                     }
 
 
+                }
+            }
+        }
+printmas(mas);*/
+
+//Задача 6.17
+//Відсортувати рядки двохвимірного масиву за зростанням.
+//Кількість рядків і стовпців вводить користувач. Масив заповнюється випадковими числами із діапазону [10,100].
+
+/*static int[,] fillmas(int[,] mas)
+{
+    Random random = new Random(2321);   
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i,j] = random.Next(10,100);
+        }
+    }
+    return mas;
+}
+Console.WriteLine("Введіть кількість стовпців");
+int colums = int.Parse(Console.ReadLine());
+Console.WriteLine("Введіть кількість рядків");
+int rows = int.Parse(Console.ReadLine());
+int[,] mas = new int[rows, colums];
+fillmas(mas);
+printmas(mas);
+int n = mas.GetLength(0);
+for(int k =0; k<mas.GetLength(0);k++){
+ for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (mas[k,j] > mas[k,j + 1])
+                {                  
+                    int temp = mas[k,j];
+                    mas[k,j] = mas[k,j + 1];
+                    mas[k,j + 1] = temp;
+                }
+            }
+        }
+}
+ printmas(mas);*/
+
+//Задача 6.18
+//Згенерувати матрицю значень n*n (квадратну). Знайти максимальний елемент у виділеному кольором діапазоні масиву:
+
+/*static int[,] fillmas(int[,] mas)
+{
+    Random random = new Random(6521);   
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i,j] = random.Next(1,100);
+        }
+    }
+    return mas;
+}
+Console.WriteLine("Введіть розмір матриці:");
+int size = int.Parse(Console.ReadLine());
+int[,] mas = new int[size, size];
+fillmas(mas);
+printmas(mas);
+int temp = size;
+int max=0;
+for (int i = 0;i < mas.GetLength(0); i++)
+{
+    for (int j = 0;j < temp; j++)
+    {
+        if (mas[i, j] > max)
+        {
+            max = mas[i, j];
+        }
+    }
+    temp--;
+}
+Console.WriteLine(max);*/
